5ca9d58abd189bef43bcbe4ef7fd56248e6f6a4c
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-10-18 16:41:51 +0100

    Created newPerson object and updated state values

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
     //This is how you can destructure and rename properties in an object.
     const { large: image } = person.picture;
     //In this case the password property is nested in the login property. This is a way you can destructure it and use it in the app.
+    //Step 1: Destructure data
     const {
       login: { password },
     } = person;
@@ -33,6 +34,19 @@ function App() {
     const {
       street: { number, name },
     } = person.location;
+
+    //Step 2: Create new object with the destructured data. if the property and the value are the same you can write only the property, there is no need to write 'image: image' because of the ES6 feature
+    const newPerson = {
+      image,
+      phone,
+      email,
+      password,
+      age,
+      street: `${number} ${name}`,
+      name: `${first} ${last}`,
+    };
+    setPerson(newPerson);
+    setLoading(false);
   };
 
   useEffect(() => {
