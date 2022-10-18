b0193dc9b1773020c4cd2ae07a53c212aa021085
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-10-18 13:52:31 +0100

    Set state values

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ import {
 const url = "https://randomuser.me/api/";
 const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [person, setPerson] = useState(null);
+  const [title, setTitle] = useState("name");
+  const [value, setValue] = useState("random person");
+
   return <h2>random user starter</h2>;
 }
 
