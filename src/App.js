dd54d842b25f67bbb34a6df113b1686ea3fd101f
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-10-18 18:14:13 +0100

    Set up UI structure

--- src/App.js
+++ src/App.js
@@ -54,8 +54,67 @@ function App() {
   useEffect(() => {
     getPerson();
   }, []);
-
-  return <h2>random user starter</h2>;
+  //If the person is not null, then look for person.image then if that is not the case then just display default (person && person.image) || defaultImage
+  const handleValue = (e) => {};
+  return (
+    <main>
+      <div className="block bcg-black"></div>
+      <div className="block">
+        <div className="container">
+          <img
+            src={(person && person.image) || defaultImage}
+            alt="random user"
+            className="user-img"
+          />
+          <p className="user-title">My {title} is </p>
+          <p className="user-value">{value}</p>
+          <div className="values-list">
+            <button
+              className="icon"
+              data-label="name"
+              onMouseOver={handleValue}
+            >
+              <FaUser />
+            </button>
+            <button
+              className="icon"
+              data-label="email"
+              onMouseOver={handleValue}
+            >
+              <FaEnvelopeOpen />
+            </button>
+            <button className="icon" data-label="age" onMouseOver={handleValue}>
+              <FaCalendarTimes />
+            </button>
+            <button
+              className="icon"
+              data-label="street"
+              onMouseOver={handleValue}
+            >
+              <FaMap />
+            </button>
+            <button
+              className="icon"
+              data-label="phone"
+              onMouseOver={handleValue}
+            >
+              <FaPhone />
+            </button>
+            <button
+              className="icon"
+              data-label="password"
+              onMouseOver={handleValue}
+            >
+              <FaLock />
+            </button>
+          </div>
+          <button className="btn" type="button">
+            {loading ? "loading..." : "random user"}
+          </button>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
